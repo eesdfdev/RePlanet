6dea8089bd8803759ffd3c11013c8f93d1d84c4b
eesdfdev <eesdfdev@gmail.com>
2024-10-09 15:01:16 +0900

    몸 애니메이션

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,31 +1,95 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    private Vector2 moveInput;
-    public Transform playerTransform;
-    public Rigidbody rb;
+    private GameObject player;
+    private Rigidbody rb;
+    private Animator animator;
+    private Animator[] animators;
+    private SpriteRenderer spriteRenderer;
+  
     public float MoveSpeed = 5f;
+    private void Start()
+    {
+        player = GetComponent<GameObject>();
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        animators = GetComponentsInChildren<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Z))
         {
             rb.velocity = Vector3.zero;
-            playerTransform.position = new Vector3(0, 2.7f, 0);
+            player.transform.position = new Vector3(0, 2.7f, 0);
         }
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Application.Quit();
         }
     }
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        moveInput.x = Input.GetAxis("Horizontal") * MoveSpeed;
-        moveInput.y = Input.GetAxis("Vertical") * MoveSpeed;
+        float horizontal = Input.GetAxis("Horizontal");
+        float vectical = Input.GetAxis("Vertical");
 
-        rb.velocity = new Vector3(moveInput.x, rb.velocity.y, moveInput.y);
-        rb.AddForce(new Vector3(0, -10, 0), ForceMode.Acceleration);
+        bool WalkingX = false;
+        bool WalkS = false;
+        bool WalkN = false;
+        bool Flip = spriteRenderer.flipX;
+        bool FlipZ = animator.GetBool("FlipZ");
+        //�ִϸ��̼� boolean ����
+        {
+            if (horizontal != 0)
+            {
+                WalkingX = true;
+                if (horizontal > 0)
+                    Flip = true;
+                else
+                    Flip = false;
+            }
+            else
+                WalkingX = false;
+            if (vectical != 0)
+            {
+                if (vectical < 0)
+                {
+                    WalkS = true;
+                    WalkN = false;
+                    FlipZ = false;
+                }
+                if (vectical > 0)
+                {
+                    WalkS = false;
+                    WalkN = true;
+                    FlipZ = true;
+                }
+            }
+            else
+            {
+                WalkN = false;
+                WalkS = false;
+            }
+        }
+        spriteRenderer.flipX = Flip;
+        animator.SetBool("WalkingX", WalkingX);
+        animator.SetBool("WalkS", WalkS);
+        animator.SetBool("WalkN", WalkN);
+        animator.SetBool("Flip", Flip);
+        animator.SetBool("FlipZ", FlipZ);
+        foreach (Animator itemAnimator in animators)
+        {
+            itemAnimator.SetBool("WalkingX", WalkingX);
+            itemAnimator.SetBool("WalkS", WalkS);
+            itemAnimator.SetBool("WalkN", WalkN);
+            itemAnimator.SetBool("Flip", Flip);
+        }
+
+
+        rb.velocity = new Vector3(horizontal * MoveSpeed, rb.velocity.y, vectical * MoveSpeed);
     }
 }
--- Assets/Scripts/GlobeManager.cs
+++ Assets/Scripts/GlobeManager.cs
@@ -1,13 +1,10 @@
 using UnityEngine;
 using UnityEngine.Rendering;
-[ExecuteAlways]
+
 public class GlobeManager : MonoBehaviour
 {
-    private void Awake()
+    private void Start()
     {
-        if (Application.isPlaying)
-            Shader.EnableKeyword("_GLOBE_ENABLED");
-        else
-            Shader.DisableKeyword("_GLOBE_ENABLED");
+        Shader.EnableKeyword("_GLOBE_ENABLED");
     }
 }
